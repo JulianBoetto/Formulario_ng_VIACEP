ac6c1f608551d171591ef38b7ac3165f6ccb415a
julián Boetto <malostratoscba@gmail.com>
2021-03-21 17:27:22 -0300

    Aula 29

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,11 @@ import { EnderecoService } from './endereco.service';
 export class AppComponent {
   
   endereco: any = {};
+  tem_endereco: boolean = false;
 
-  constructor(private endeServ: EnderecoService) {}
+  constructor(private endeServ: EnderecoService) {
+    this.tem_endereco = false;
+  }
 
   onSubmit(formulario: any) {
     if(formulario.form.status == 'INVALID') {
@@ -21,9 +24,15 @@ export class AppComponent {
 
     this.endeServ.getEndereco(formulario.form.value.zipcode).subscribe(endereco => {
       this.endereco = endereco;
+      this.tem_endereco = true;
       console.log(this.endereco);
     });
   
     console.log(formulario.form.value);
+  }
+
+  limpar() {
+    this.endereco = {};
+    this.tem_endereco = false;
   }
 }
